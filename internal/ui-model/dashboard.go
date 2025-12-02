eed08a79d058e1eac4927d17bdc86a8b2e57ad97
Dave P <davep1405@gmail.com>
2025-12-02 12:57:43 +0700

    fix: wrong package name

--- internal/ui-model/dashboard.go
+++ internal/ui-model/dashboard.go
@@ -1,11 +1,14 @@
 package uimodel
 
-import "github.com/d4ve-p/clonis/internal/database"
+import (
+	"github.com/d4ve-p/clonis/internal/database"
+	"github.com/d4ve-p/clonis/internal/model"
+)
 
 type DashboardData struct {
 	User bool
 	Paths []database.Path
-	Logs []database.LogEntry
+	Logs []model.LogEntry
 	Settings map[string]string
 	DriveLinked bool
 }
