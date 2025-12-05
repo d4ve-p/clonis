541e224c88e8cfe7d56c8cc287b9999a71516208
Dave P <davep1405@gmail.com>
2025-12-05 13:03:43 +0700

    fix: changed sqlite library with simpler dependency

--- internal/database/store.go
+++ internal/database/store.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 type Store struct {
@@ -25,7 +25,7 @@ func GetDatabase() (*Store, error) {
 		return nil, err
 	}
 
-	db, err := sql.Open("sqlite3", dbPath)
+	db, err := sql.Open("sqlite", dbPath)
 
 	if err != nil {
 		return nil, err
